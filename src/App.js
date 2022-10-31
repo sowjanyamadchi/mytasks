e7ce6a5d39e94165e41c2d516112a7c98b4bb63e
sowjanyamadchi <sowjanya.madchi1@gmail.com>
2022-10-31 15:21:00 +0000

    mytasks

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import './App.css'
+import Home from './components/MyTaskHome'
+
+// These are the lists used in the application. You can move them to any component needed.
+/* eslint-disable */
+
+const tagsList = [
+  {
+    optionId: 'HEALTH',
+    displayText: 'Health',
+  },
+  {
+    optionId: 'EDUCATION',
+    displayText: 'Education',
+  },
+  {
+    optionId: 'ENTERTAINMENT',
+    displayText: 'Entertainment',
+  },
+  {
+    optionId: 'SPORTS',
+    displayText: 'Sports',
+  },
+  {
+    optionId: 'TRAVEL',
+    displayText: 'Travel',
+  },
+  {
+    optionId: 'OTHERS',
+    displayText: 'Others',
+  },
+]
+
+// Replace your code here
+
+const App = () => <Home />
+
+export default App
